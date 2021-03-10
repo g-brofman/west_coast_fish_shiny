f4c23f34ffaf3bbe9a2f9760b2abd104f54b16b1
dglave <glave@ucsb.edu>
2021-03-09 19:28:56 -0800

    I have a wrangled comparison of the Gulf and WC fish data

--- grave/gulf_compare.R
+++ grave/gulf_compare.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(here)
+library(fmsb)
+library(stats)
+library(wesanderson)
 
 ## Loading in Guld and WC Data
 
@@ -9,18 +12,42 @@ fish_gulf <- read_csv(here("west_coast_eez_data", "SAU EEZ 852 v48-0.csv"))
 
 ## We'll join these two df by the column "area_name"
 
-all_fish <- full_join(fish, fish_gulf, by = c("area_name", "area_type", "year", # Do all useful ones!))
+all_fish <- full_join(fish, fish_gulf, by = c("area_name",
+                                              "area_type",
+                                              "year",
+                                              "common_name",
+                                              "functional_group",
+                                              "commercial_group",
+                                              "fishing_sector",
+                                              "reporting_status",
+                                              "gear_type",
+                                              "tonnes",
+                                              "landed_value")) %>%
+  select(area_name, area_type, year, common_name, functional_group, commercial_group, fishing_sector, reporting_status, gear_type, tonnes, landed_value)
+                                              # Do all useful ones!))
 
 
+## Below are two lengthy wrangling steps to get two rows for each commercial group of fish. One for each area (Gulf of Mexico, West Coast.) These rows include the percentage reported, tonnes, and landed value.
 
-# radarchart(fish, axistype=1 ,
-#
-#             #custom polygon
-#             pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 ,
-#
-#             #custom the grid
-#             cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
-#
-#             #custom labels
-#             vlcex=0.8
-# )
+fish_counts <- all_fish %>%
+  group_by(commercial_group, area_name, landed_value, tonnes) %>%
+  count(reporting_status) %>%
+  pivot_wider(names_from = reporting_status, values_from = n)
+
+fish_counts_summarized <- fish_counts %>%
+  group_by(commercial_group, area_name, Unreported, Reported) %>%
+  summarize(landed_value = sum(landed_value, na.rm = TRUE),
+            tonnes = sum(tonnes, na.rm = TRUE),
+            Unreported = sum(Unreported, na.rm = TRUE),
+            Reported = sum(Reported, na.rm = TRUE)) %>%
+  summarize()
+
+summary_2 <- fish_counts_summarized %>%
+  group_by(commercial_group, area_name) %>%
+  summarize(Unreported = sum(Unreported, na.rm = TRUE),
+            Reported = sum(Reported, na.rm = TRUE),
+            landed_value = sum(landed_value, na.rm = TRUE),
+            tonnes = sum(tonnes, na.rm = TRUE)) %>%
+  mutate(percent_reported = Reported/(Reported+Unreported)*100) %>%
+  select(commercial_group, area_name, tonnes, landed_value, percent_reported)
+
