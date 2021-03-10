81d7eb9013bacb8caaefd5e406eed7ba80ce5e14
grace-brofman <gracebrofman@bren.ucsb.edu>
2021-03-10 14:49:42 -0800

    added fish landings by tonnes over time graph

--- grave/app.R
+++ grave/app.R
@@ -17,34 +17,80 @@ library(treemap)
 library(patchwork)
 library(GGally)
 library(viridis)
-
+library(fmsb)
+library(stats)
 library(d3Tree)
 library(ECharts2Shiny)
 library(here)
 
 #hrbrthemes::import_roboto_condensed()
 
-#d fish <- data.table::fread(here("grave", "west_coast_eez_data", "SAU EEZ 848 v48-0.csv"))
-
-#d westcoast_eez_raw <- read_csv(here("grave", "west_coast_eez_data", "SAU EEZ 848 v48-0.csv"))
-
 
 # -----------------------------------------------------------------
-fish <- read_csv(here("west_coast_eez_data","SAU EEZ 848 v48-0.csv")) %>%
+fish_1 <- read_csv(here("west_coast_eez_data","SAU EEZ 848 v48-0.csv")) %>%
     select(4:16)
 
-fish_by_gear <- fish %>%
+fish_annual <- fish_1 %>%
+  group_by(year) %>%
+  summarize(tonnes = sum(tonnes))
+
+fish_by_gear <- fish_1 %>%
     select(gear_type, tonnes, landed_value, commercial_group, common_name)
 
 # species by year, landed value, and gear_type
-fish_category_gear <- fish %>%
+fish_category_gear <- fish_1 %>%
     group_by(year, common_name) %>%
     summarize(landed_value = sum(landed_value))
 
 #fish_category_gear$year <- as.Date(fish_category_gear$year, "%Y")
 
+# --------- Code for E vs. W Coast Comparison ---------------------
 
-# -----------------------------------------------------------------
+fish <- read_csv(here("west_coast_eez_data", "SAU EEZ 848 v48-0.csv"))
+fish_gulf <- read_csv(here("west_coast_eez_data", "SAU EEZ 852 v48-0.csv"))
+
+
+## We'll join these two df by the column "area_name"
+
+all_fish <- full_join(fish, fish_gulf, by = c("area_name",
+                                              "area_type",
+                                              "year",
+                                              "common_name",
+                                              "functional_group",
+                                              "commercial_group",
+                                              "fishing_sector",
+                                              "reporting_status",
+                                              "gear_type",
+                                              "tonnes",
+                                              "landed_value")) %>%
+  select(area_name, area_type, year, common_name, functional_group, commercial_group, fishing_sector, reporting_status, gear_type, tonnes, landed_value)
+# Do all useful ones!))
+
+
+## Below are two lengthy wrangling steps to get two rows for each commercial group of fish. One for each area (Gulf of Mexico, West Coast.) These rows include the percentage reported, tonnes, and landed value.
+
+fish_counts <- all_fish %>%
+  group_by(commercial_group, area_name, landed_value, tonnes) %>%
+  count(reporting_status) %>%
+  pivot_wider(names_from = reporting_status, values_from = n)
+
+fish_counts_summarized <- fish_counts %>%
+  group_by(commercial_group, area_name, Unreported, Reported) %>%
+  summarize(landed_value = sum(landed_value, na.rm = TRUE),
+            tonnes = sum(tonnes, na.rm = TRUE),
+            Unreported = sum(Unreported, na.rm = TRUE),
+            Reported = sum(Reported, na.rm = TRUE))
+
+summary_2 <- fish_counts_summarized %>%
+  group_by(commercial_group, area_name) %>%
+  summarize(Unreported = sum(Unreported, na.rm = TRUE),
+            Reported = sum(Reported, na.rm = TRUE),
+            landed_value = sum(landed_value, na.rm = TRUE),
+            tonnes = sum(tonnes, na.rm = TRUE)) %>%
+  mutate(percent_reported = Reported/(Reported+Unreported)*100) %>%
+  select(commercial_group, area_name, tonnes, landed_value, percent_reported)
+
+# -------------------end of E vs. W code wrangling------------------------
 
 
 my_theme <- bs_theme(
@@ -82,7 +128,7 @@ ui <- dashboardPage(skin = "red",
                       tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
                     ), #end of tags$head
                         fluidPage(theme = my_theme,
-                                  h1("Visualizing Fish Landings in the Pacific Coast Region EEZ"), #header on all tabs
+                                  h1("Visualizing Fish Landings in the Pacific Coast Region EEZ") #header on all tabs
                                  # p("Fish tend to have two eyes", #subheader on all tabs
                                   #  a("What IS a fish exactly?", #subheader on all tabs
                                   #    href = "https://en.wikipedia.org/wiki/Fish")#end of a
@@ -118,8 +164,6 @@ ui <- dashboardPage(skin = "red",
                                                             hr(),
                                                             fluidRow(column(3, verbatimTextOutput("value_tbd")))) #(where radio buttons are specified)
 
-
-
                                     ) #end of fluidPage
                             ), # end of tabItem2
 
@@ -153,14 +197,16 @@ ui <- dashboardPage(skin = "red",
 
                                                             ), #end of box
 
-
-
                                         shinydashboard::box(plotOutput(outputId = "fish_plot", height = 300, width = 700)#end of plotOutput
                                         ),#end of box
+       h4("second plot title"),
+                                        shinydashboard::box(plotOutput(outputId = "fish_plot2", height = 300, width = 700)) #end of plotOutput
 
+# ----------- box 2 will start here
                                     ),#end of fluidRow
        p("description here")
                             ),#end of tabItem3
+
 
                             tabItem(tabName = "tree_graph_tab",
                                     h3("Descriptive subtitle here"),
@@ -175,13 +221,14 @@ ui <- dashboardPage(skin = "red",
           fluidRow(column(1)
           # verbatimTextOutput("landed_value")) # changed br to hr, just to see.
                                                             )),#end of box
-      h4(""),
-   shinydashboard::box(plotOutput(outputId = "fish_tree", width = 600),   #end of plotOutput
+      img(src = "gillnet.png", height = 210, width = 320),
+
+   shinydashboard::box(plotOutput(outputId = "fish_tree", width = 750)   #end of plotOutput
    #                     source(file = "treemap.R",
    #                            local = TRUE),    #end of source()
-                       ), #end of box
-                                    ), #end of fluidRow
-   p("description here")
+                       ) #end of box
+
+) #end of fluidRow
                             ) #end of tabItem4
                         ) #end of tabItems
 
@@ -202,7 +249,14 @@ server <- function(input, output) {
 
     }#end of reactive({})
     )#end of reactive
-    # where end of first {} used to be before adding output section
+
+    # second reactive df for fish tab: fish catch by tons over time
+
+    fish_select2 <- reactive({
+      fish_annual %>%
+        filter(year >= input$slider2[1], year <= input$slider2[2] )
+
+    }) # end of reactive({})
 
     # Create a reactive plot, which depends on 'species' widget selection:
 
@@ -217,6 +271,20 @@ server <- function(input, output) {
              title = "Fish catch by landed value over time")
 
     })
+    # second reactive plot, same data
+    output$fish_plot2 <- renderPlot({
+
+      ggplot(data = fish_select2(),
+             aes(x = year, y = tonnes)) +
+        geom_point(color = "red") +
+        geom_smooth(color = "blue") +
+        theme_minimal() +
+        labs(x = "Year",
+             y = "Landed Tonnes",
+             title = "Fish catch by landed tonnes over time")
+    })
+
+
 
 
 
@@ -249,12 +317,8 @@ gear_filtered <- reactive({
 
  #   output$value <- renderPrint({ input$checkGroup })
 
-
 } # End of server squigglies
 
-
-
-#### End of sort-of-working app
 
 
 
