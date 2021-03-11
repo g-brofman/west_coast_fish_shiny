2b587abfecbf67e665c09642d64b9494f96e1b70
dglave <glave@ucsb.edu>
2021-03-11 15:29:14 -0800

    I made the Sector widget shine!

--- grave/app.R
+++ grave/app.R
@@ -70,25 +70,30 @@ all_fish <- full_join(fish, fish_gulf, by = c("area_name",
 ## Below are two lengthy wrangling steps to get two rows for each commercial group of fish. One for each area (Gulf of Mexico, West Coast.) These rows include the percentage reported, tonnes, and landed value.
 
 fish_counts <- all_fish %>%
-  group_by(commercial_group, area_name, landed_value, tonnes) %>%
+  group_by(commercial_group, area_name, landed_value, tonnes, fishing_sector) %>%
   count(reporting_status) %>%
   pivot_wider(names_from = reporting_status, values_from = n)
 
 fish_counts_summarized <- fish_counts %>%
-  group_by(commercial_group, area_name, Unreported, Reported) %>%
+  group_by(commercial_group, area_name, Unreported, Reported, fishing_sector) %>%
   summarize(landed_value = sum(landed_value, na.rm = TRUE),
             tonnes = sum(tonnes, na.rm = TRUE),
             Unreported = sum(Unreported, na.rm = TRUE),
             Reported = sum(Reported, na.rm = TRUE))
 
 summary_2 <- fish_counts_summarized %>%
-  group_by(commercial_group, area_name) %>%
+  group_by(commercial_group, area_name, fishing_sector) %>%
   summarize(Unreported = sum(Unreported, na.rm = TRUE),
             Reported = sum(Reported, na.rm = TRUE),
             landed_value = sum(landed_value, na.rm = TRUE),
             tonnes = sum(tonnes, na.rm = TRUE)) %>%
   mutate(percent_reported = Reported/(Reported+Unreported)*100) %>%
-  select(commercial_group, area_name, tonnes, landed_value, percent_reported)
+  select(commercial_group, area_name, tonnes, landed_value, percent_reported, fishing_sector)
+
+summary_factor <- summary_2 %>%
+  mutate(commercial_group = as.factor(commercial_group)) %>%
+  mutate(area_name = as.factor(area_name)) %>%
+  mutate(fishing_sector = as.factor(fishing_sector))
 
 # -------------------end of E vs. W code wrangling------------------------
 
@@ -155,17 +160,17 @@ ui <- dashboardPage(skin = "red",
                             tabItem(tabName = "fishermen_tab",
                                     h3("East vs. West Coast EEZ Comparison"),
                                     fluidPage(
-                                      shinydashboard::box(plotOutput(outputId = "e_w_plot", height = 300, width = 700)#end of plotOutput
+                                      shinydashboard::box(plotOutput(outputId = "e_w_plot", width = 1000)#end of plotOutput
                                                           ), # end of box (where plot will go)
-                                      shinydashboard::box(checkboxGroupInput("checkGroup", label = h3("Select fishing sector"),
-                                                                            all_fish$fishing_sector,
-     choices = list("Artisanal" = 1, "Industrial" = 2, "Recreational" = 3),
-                                                                         selected = 1),
-
+                                      shinydashboard::box(radioButtons(
+                                        inputId = "sector_choice",
+                                        label = "Choose one Fishing Sector",
+                                        choices = c("Industrial", "Artisanal", "Recreational"),
+                                        selected = "Industrial"),
 
 
                                                             hr(),
-                                                            fluidRow(column(3, verbatimTextOutput("value_tbd")))) #(where radio buttons are specified)
+                                                            fluidRow(column(3, verbatimTextOutput("fishing_sector")))) #(where radio buttons are specified)
 
                                     ) #end of fluidPage
                             ), # end of tabItem2
@@ -291,8 +296,8 @@ server <- function(input, output) {
 
 
 sector_selected <- reactive({
-  all_fish %>%
-    filter(fishing_sector == input$fishing_sector)
+  summary_factor %>%
+    filter(fishing_sector == input$sector_choice)
 }) #End or sector_selected
 
 
@@ -306,7 +311,7 @@ gear_filtered <- reactive({
 
 
 output$e_w_plot <- renderPlot({
-  e_w_plot <- summary_factor %>%
+  sector_selected()  %>%
     ggparcoord(
       columns = 3:5, groupColumn = 2, order = "anyClass",
       showPoints = TRUE,
@@ -316,9 +321,19 @@ output$e_w_plot <- renderPlot({
     scale_color_discrete(c("blue", "red")) +
     theme_ipsum()+
     theme(
-      plot.title = element_text(size=10)
-    )
+      plot.title = element_text(size=10),
+      legend.title = element_blank()) +
+    labs(x = "Fishing variables",
+         y = "Relative Scale")
+
+
 }) # end of of renderPlot squiggles
+
+## Output for radio button (region comparison)
+output$txt <- renderText({
+  paste("You chose", input$sector_choice)
+})
+
 
 
     ## Creating a reactive treeplot
